99a6041a29873500f61ca3736abeb56eb3c6b31f
20aszulc <33702922+20aszulc@users.noreply.github.com>
2017-11-23 17:14:40 -0800

    Update Sketching.py

--- Sketching.py
+++ Sketching.py
@@ -3,8 +3,10 @@ import random
 hey=tk.Tk()
 #DONT LOOK BELOW PLZ
 culurs=["green", "blue", "red", "black", "brown", "coral", "cyan", "chocolate", "DarkOrchid", "cornsilk2", "khaki", "honeydew1", "HotPink"]
-canvas=tk.Canvas(hey, bg=random.choice(culurs), height=250, width=300)
-canvas.grid(row=1, column=1)
+canvasheight=250
+canvaswidth=300
+canvas=tk.Canvas(hey, bg=random.choice(culurs), height=canvasheight, width=canvaswidth)
+canvas.grid(rowspan=1, columnspan=10)
 ball_radius=random.randint(5,25)
 ball=[]
 #ball.append((event.x,event.y))
@@ -17,7 +19,7 @@ def switchroles2():
     type="circle"
 def switchroles3():
     global type
-    type="square"
+    type="coolsquare"
 def switchroles4():
     global type
     type="triangle"
@@ -26,8 +28,8 @@ line=tk.Button(hey, text="line", command=switchroles1,)
 line.grid(row=1, column =2)
 circle=tk.Button(hey, text="cirlce", command=switchroles2,)
 circle.grid(row=1, column =5)
-square=tk.Button(hey, text="square", command=switchroles3,)
-square.grid(row=1, column =3)
+coolsquare=tk.Button(hey, text="coolsquare", command=switchroles3,)
+coolsquare.grid(row=1, column =3)
 triangle=tk.Button(hey, text="triangle", command=switchroles4,)
 triangle.grid(row=1, column =4)
 def click(event):
@@ -43,15 +45,16 @@ def click(event):
                                       event.x+ball_radius, event.y+ball_radius,
                                     fill=random.choice(culurs),  width=1,)
         #ball.pop()
-    if type=="square":
+    if type=="coolsquare":
         #missing a second postion. Need 2nd click to create actual line accomplish
         #have x1 and x2 and find difference between the two
         #ex:abs(event.x-event.x2)
         #make event.x2=items in ball[]. But still be an integer. Ex: ball[3]=event.x2. Event.x2 might be substituted for actual variable
         print("yo")
-        squareitem = canvas.create_rectangle(abs(event.x-event.x2), abs(event.y-event.x2),
-                                             abs (event.x - event.x2), abs(event.y-event.x2),
-                                             fill=random.choice(culurs),  width=1,)
+        canvas.create_rectangle (event.x, event.y, canvaswidth/2, canvasheight/2, fill=random.choice(culurs))
+        #squareitem = canvas.create_rectangle(#abs(event.x-event.x2), abs(event.y-event.x2),
+                                             #abs (event.x - event.x2), abs(event.y-event.x2),
+                                             #fill=random.choice(culurs),  width=1,)
     if type=="triangle":
         print("what up")
         if len (ball)== 3:
@@ -60,11 +63,35 @@ def click(event):
                                     fill=random.choice(culurs),  width=1,)
 
 #othershapes
-square.bind ("<Button-1>", click)
+coolsquare.bind ("<Button-1>", click)
 triangle.bind ("<Button-1>", click)
 canvas.bind("<Button-1>", click)
 line.bind ("<Button-1>", click)
 circle.bind ("<Button-1>", click)
+
+def settings():
+    global input1
+    input1=input("what size of shape?")
+    if input1>="1" and input1<="50":
+        ball_radius=int(input1)
+    input1= input("what colors?")
+    if input1 == "green" "blue" "red" "black" "brown" "coral" "cyan" "chocolate" "DarkOrchid" "cornsilk2" "khaki" "honeydew1" "HotPink":
+        culurs.pop()
+        culurs=input1
+    input1=input ("what width of canvas?")
+    if input1>="100" and input1<="2000":
+        canvaswidth=int(input1)
+    input1 = input ("what height of canvas?")
+    if input1<="100" and input1<="2000":
+        canvasheight=int(input1)
+        canvas = tk.Canvas (hey, bg=random.choice (culurs), height=canvasheight, width=canvaswidth)
+        canvas.grid (rowspan=1, columnspan=10)
+
+settings = tk.Button (hey, text="Advanced Settings", command=settings, )
+settings.grid (row=5, column=5)
+quit = settings = tk.Button (hey, text="Quit", command=hey.destroy, )
+quit.grid (row=4, column=5)
+
 
 hey.mainloop()
 
