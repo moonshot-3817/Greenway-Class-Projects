4e08325b5091a2ffffbbea5c36f20ae68e2c7d10
20aszulc <33702922+20aszulc@users.noreply.github.com>
2017-11-23 20:36:27 -0800

    Update Sketching.py

--- Sketching.py
+++ Sketching.py
@@ -1,10 +1,10 @@
-import tkinter as tk
+import Tkinter as tk
 import random
 hey=tk.Tk()
 #DONT LOOK BELOW PLZ
 culurs=["green", "blue", "red", "black", "brown", "coral", "cyan", "chocolate", "DarkOrchid", "cornsilk2", "khaki", "honeydew1", "HotPink"]
-canvasheight=250
-canvaswidth=300
+canvasheight=500
+canvaswidth=1000
 canvas=tk.Canvas(hey, bg=random.choice(culurs), height=canvasheight, width=canvaswidth)
 canvas.grid(rowspan=1, columnspan=10)
 ball_radius=random.randint(5,25)
@@ -23,10 +23,26 @@ def switchroles3():
 def switchroles4():
     global type
     type="triangle"
+def switchroles5():
+    global type
+    type="square"
+def switchroles6():
+    global type
+    type="paint"
+def switchroles7():
+    global type
+    type="verticalline"
+def switchroles8():
+    global type
+    type="horizontalline"
 
 line=tk.Button(hey, text="line", command=switchroles1,)
 line.grid(row=1, column =2)
-circle=tk.Button(hey, text="cirlce", command=switchroles2,)
+verticalline=tk.Button(hey, text="verticalline", command=switchroles7,)
+verticalline.grid(row=1, column =9)
+horizontalline=tk.Button(hey, text="horizontalline", command=switchroles8,)
+horizontalline.grid(row=1, column =6)
+circle=tk.Button(hey, text="circle", command=switchroles2,)
 circle.grid(row=1, column =5)
 coolsquare=tk.Button(hey, text="coolsquare", command=switchroles3,)
 coolsquare.grid(row=1, column =3)
@@ -34,6 +50,8 @@ square=tk.Button(hey, text="square", command=switchroles5,)
 square.grid(row=1, column =7)
 triangle=tk.Button(hey, text="triangle", command=switchroles4,)
 triangle.grid(row=1, column =4)
+paint=tk.Button(hey, text="paint", command=switchroles6,)
+paint.grid(row=1, column =8)
 def click(event):
     if type=="circle":
         print("hi")
@@ -43,9 +61,19 @@ def click(event):
     #code coome from https://stackoverflow.com/questions/28615900/how-do-i-add-a-mouse-click-position-to-a-list-in-tkinter
     if type=="line":
         #ball.append ([event.x, event.y])
-        lineitem = canvas.create_line(event.x-ball_radius, event.y-ball_radius,
-                                      event.x+ball_radius, event.y+ball_radius,
+        lineitem = canvas.create_line(event.x-ball_radius, event.y- ball_radius,
+                                      event.x+ball_radius, event.y+ ball_radius,
                                     fill=random.choice(culurs),  width=1,)
+    if type == "horizontalline":
+            # ball.append ([event.x, event.y])
+        lineitem = canvas.create_line(event.x - ball_radius, event.y,
+                                          event.x + ball_radius, event.y,
+                                          fill=random.choice(culurs), width=1, )
+    if type == "verticalline":
+            # ball.append ([event.x, event.y])
+        lineitem = canvas.create_line(event.x, event.y - ball_radius,
+                                    event.x , event.y+ ball_radius,
+                                        fill=random.choice(culurs), width=1, )
         #ball.pop()
     if type=="coolsquare":
         #missing a second postion. Need 2nd click to create actual line accomplish
@@ -59,31 +87,39 @@ def click(event):
                                              #fill=random.choice(culurs),  width=1,)
     if type=="square":
         print("sigh")
-             x1, y1 = ( event.x - 1 ), ( event.y - 1 )
-             x2, y2 = ( event.x + 1 ), ( event.y + 1 )
-            canvas.create_rectangle (event.x, event.y, canvaswidth/2, canvasheight/2, fill=random.choice(culurs))
+        cat=random.randint(1, 250)
+        x1, y1 = ( event.x - cat ), ( event.y - cat )
+        x2, y2 = ( event.x + cat ), ( event.y + cat )
+        canvas.create_rectangle (x1, y1, x2, y2, fill=random.choice(culurs))
     if type=="triangle":
         print("what up")
         if len (ball)== 3:
             triangleitem = canvas.create_polygon(event.x-ball_radius, event.y-ball_radius,
                                     event.x+ball_radius, event.y+ball_radius,
                                     fill=random.choice(culurs),  width=1,)
-    def paint( event ):
-        inputcolors=input("What color?")
-        x1, y1 = ( event.x - 1 ), ( event.y - 1 )
-        x2, y2 = ( event.x + 1 ), ( event.y + 1 )
-        canvas.create_oval( x1, y1, x2, y2, fill = inputcolors)
-        canvas.bind( "<B1-Motion>", paint )
-    if type=="paint":
-        paint( event)
+
+def paint( event ):
+    if type == "paint":
+        believe=0
+        while believe <100:
+            x1, y1 = ( event.x - 3 ), ( event.y - 3 )
+            x2, y2 = ( event.x + 3 ), ( event.y + 3 )
+            lineitem = canvas.create_line(event.x, event.y - ball_radius,
+                                    event.x , event.y+ ball_radius,
+                                        fill=random.choice(culurs), width=1, )
+            believe=+ 1
+
 
 #othershapes
 coolsquare.bind ("<Button-1>", click)
 triangle.bind ("<Button-1>", click)
 canvas.bind("<Button-1>", click)
 line.bind ("<Button-1>", click)
+verticalline.bind ("<Button-1>", click)
+horizontalline.bind ("<Button-1>", click)
 circle.bind ("<Button-1>", click)
 square.bind("<Button-1>", click)
+canvas.bind("<B1-Motion>", paint)
 
 def settings():
     global input1
@@ -110,88 +146,4 @@ quit.grid (row=4, column=5)
 
 
 hey.mainloop()
-
-'''''''''
-import math
-try:
-    from tkinter import *
-except ImportError:
-    from Tkinter import *
-
-# intialize globals
-width = 450
-height = 300
-ball_list = []
-ball_radius = 15
-
-# helper function
-def distance(p, q):
-    return math.sqrt((p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2)
-
-# define event handler for mouse click, draw  (simplegui)
-"""
-def click(pos):
-    changed = False
-    for ball in ball_list:
-        if distance([ball[0], ball[1]], pos) < ball_radius:
-            ball[2] = "Green"
-            changed = True
-
-    if not changed:
-        ball_list.append([pos[0], pos[1], "Red"])
-"""
-
-def click(event):
-    """ replacement mouse handler inside Canvas, draws a red ball on each click"""
-    changed = False
-    print ("Canvas: mouse clicked at ", event.x, event.y)
-    for ball in ball_list:
-        if distance([ball[0], ball[1]], [event.x, event.y]) < ball_radius:
-            ball[2] = "Green"
-            changed = True       #  I didn't use this yet
-
-    if not changed:
-        ball_list.append([event.x, event.y, "Red"])  # didn't use this either
-
-    canvas.create_oval(event.x - ball_radius, event.y - ball_radius, \
-                       event.x + ball_radius, event.y + ball_radius,\
-                       width=2, fill= "Red")
-
-
-def callback_frame(event):
-    """ handler for mouse click in the Frame instead of the Canvas """
-    print ("Frame: mouse clicked at", event.x, event.y)
-
-"""
-don't need a draw handler because the balls aren't moving
-def draw(canvas):
-    for ball in ball_list:
-        canvas.draw_circle([ball[0], ball[1]], ball_radius, 1, "Black", ball[2])
-"""
-
-# create frame
-#frame = simplegui.create_frame("Mouse selection", width, height)
-#frame.set_canvas_background("White")
-
-# register event handler
-#frame.set_mouseclick_handler(click)
-#frame.set_draw_handler(draw)
-
-# start frame
-#frame.start()
-
-root = Tk()
-
-root.title('Mouse click makes red ball')
-frame = Frame(root, width = 200, height = 200)
-frame.grid(column = 0, row = 1)
-
-canvas = Canvas(root,width=200, height=200, bg='white')
-canvas.grid(column = 1, row = 1)
-
-frame.bind("<Button-1>", callback_frame)
-canvas.bind("<Button-1>", click)
-
-mainloop()
-'''''
 #http://www.codeskulptor.org/#user14_MxIPA78aRmnIJU3.py
